7c7495ea627c822ca1446037323dd02dcf2ef2dc
João Flores <j199818@dac.unicamp.br>
2021-10-19 03:57:31 -0300

    up

--- luxi-starter-master/components/Feature/feature-style.js
+++ luxi-starter-master/components/Feature/feature-style.js
@@ -72,93 +72,16 @@ const featureStyles = makeStyles(theme => ({
       marginBottom: 0,
     }
   },
-  deco1: {
-    ...decoration(theme)
-  },
-  deco2: {
-    ...decoration(theme),
-    width: 450,
-    height: 450,
-    top: 40,
-    left: 'calc(50% - 350px)',
-  },
-  parallaxWrap: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    overflow: 'hidden',
-    top: 0,
-    left: 0,
-    zIndex: 0,
-    [theme.breakpoints.down('md')]: {
-      display: 'none'
-    },
-    '& figure > div': {
-      height: 1000,
-      width: '100%',
-      position: 'relative',
-      top: 500,
-    }
-  },
-  bannerParallaxWrap: {
-    height: 800,
-    width: '100%',
-    position: 'absolute',
-    display: 'block',
-    '& figure': {
-      height: 800,
-      width: '100%',
-      display: 'block',
-      position: 'absolute',
-    },
-    '& figure > div': {
-      height: 800,
-      width: '100%',
-      display: 'block',
-      position: 'absolute',
-      top: 0,
-    }
-  },
-  parallaxVertical: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    [theme.breakpoints.up('lg')]: {
-      transform: 'scale(0.3)'
-    },
-    [theme.breakpoints.up('xl')]: {
-      display: 'none'
-    },
-  },
-  parallaxDot: {
-    top: -20,
-    fill: theme.palette.text.hint,
-    width: 845,
-    height: 1099,
-    opacity: 0.4,
-    left: -370
-  },
-  parallaxTriangle: {
-    top: 100,
-    outline: theme.palette.text.hint,
-    opacity: 0.1,
-    width: 902,
-    height: 1042,
-    stroke: theme.palette.text.hint,
-    fill: 'transparent',
-    strokeWidth: 50,
-    right: -210
-  },
-  parallaxCircle: {
-    top: 250,
-    width: 600,
-    height: 570,
-    opacity: 0.1,
-    stroke: theme.palette.text.hint,
-    fill: 'transparent',
-    strokeWidth: 40,
-    right: 40
-  },
+  // deco1: {
+  //   ...decoration(theme)
+  // },
+  // deco2: {
+  //   ...decoration(theme),
+  //   width: 450,
+  //   height: 450,
+  //   top: 40,
+  //   left: 'calc(50% - 350px)',
+  // }
 }));
 
 export default featureStyles;
