dbee6f73f292c895aa1f893f81df366b096aa1da
João Flores <j199818@dac.unicamp.br>
2021-11-20 18:23:58 -0300

    ajustes de layout

--- MC855/luxi-starter-master/pages/posts/first-post.js
+++ MC855/luxi-starter-master/pages/posts/first-post.js
@@ -30,46 +30,49 @@ import TextField from '@material-ui/core/TextField';
 
 const sectionMargin = margin => (margin * 15);
 const useStyles = makeStyles(theme => ({
-  mainWrap: {
-    position: 'relative',
-    width: '100%',
-    overflow: 'hidden',
-  },
-  spaceBottom: {
-    marginBottom: 0
-  },
-  spaceTop: {
-    paddingTop: sectionMargin(theme.spacing()),
-    background: "#ffffff"
-  },
-  containerWrap: {
-    marginTop: theme.spacing(10)
-  },
+    mainWrap: {
+        position: 'relative',
+        width: '100%',
+        overflow: 'hidden',
+    },
+    spaceBottom: {
+        marginBottom: 0
+    },
+    spaceTop: {
+        paddingTop: sectionMargin(theme.spacing()),
+        background: "#ffffff"
+    },
+    containerWrap: {
+        marginTop: theme.spacing(10)
+    },
 }));
 
 function FirstPost() {
-  const classes = useStyles();
-  return (
-    <React.Fragment>
-      <Head >
-        <title>
-          {brand.starter.name}
-          &nbsp; - Adicionar
-        </title>
-      </Head>
-      <CssBaseline />
-      <section id="Add" />
-      <div className={classes.mainWrap}>
-        <Header />
-        <div className="container">
-          <p>Início / Catálogo / Ofertar um item</p>
-          <h1 className="title">
-            <Link href="/">
-              <a>←</a>
-            </Link>
-            {' '}Ofertar um item
-          </h1>
-          <style jsx>{`
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <Head >
+                <title>
+                    {brand.starter.name}
+                    &nbsp; - Adicionar
+                </title>
+            </Head>
+            <CssBaseline />
+            <section id="Add" />
+            <div className={classes.mainWrap}>
+                <Header />
+                <main className={classes.containerWrap}>
+                    <div className={clsx(classes.featureMore)}>
+                        <div className="container">
+                            <br/>
+                            <p>Início / Catálogo / Ofertar um item</p>
+                            <h1 className="title">
+                                <Link href="/">
+                                    <a>←</a>
+                                </Link>
+                                {' '}Ofertar um item
+                            </h1>
+                            <style jsx>{`
             .container {
               margin: 50px;
             }
@@ -77,115 +80,84 @@ function FirstPost() {
               color: gray;
             }
           `}</style>
-        </div>
-        <Grid container spacing={6} className={classes.backgroundd}>
-          <Grid md={6} >
-            <FirstPostLeftGrid />
-          </Grid>
-          <Grid md={6} >
-            <FirstPostRightGrid />
-          </Grid>
-        </Grid>
-      </div>
-    </React.Fragment>
-  )
+                        </div>
+                        <Grid container spacing={6} className={classes.backgroundd}>
+                            <Grid md={6} >
+                                <NameForm/>
+                            </Grid>
+                            <Grid md={6} >
+                                <NameForm/>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </main>
+            </div>
+        </React.Fragment>
+    )
 }
 
 FirstPost.getInitialProps = async () => ({
-  namespacesRequired: ['common'],
+    namespacesRequired: ['common'],
 });
 
 FirstPost.propTypes = {
-  onToggleDark: PropTypes.func.isRequired,
-  onToggleDir: PropTypes.func.isRequired,
+    onToggleDark: PropTypes.func.isRequired,
+    onToggleDir: PropTypes.func.isRequired,
 };
 
 export default FirstPost;
 
-class FirstPostLeftGrid extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <Container maxWidth="md">
-          <label>
-            Nome do item
-          </label>
-          <form>
-            <textarea />
-          </form>
-        </Container>
 
-        <Container maxWidth="md">
-          <label>
-            Descrição do anúncio
-          </label>
-          <form>
-            <textarea />
-          </form>
-          <p>
-            Atenção: Não adicione seus dados de contato à descrição pública do
-            anúncio. Ao invés disso, adicione uma forma de contato ao seu perfil
-            de usuário aqui.
-          </p>
-        </Container>
-        <style jsx>{`
+
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                                    <Container maxWidth="md">
+                                        <label>
+                                            Nome do item
+                                        </label>
+                                        <form>
+                                            <textarea />
+                                        </form>
+                                    </Container>
+
+                                    <Container maxWidth="md">
+                                        <label>
+                                            Descrição do anúncio
+                                        </label>
+                                        <form>
+                                            <textarea>
+
+                                            </textarea>
+                                        </form>
+                                    </Container>
+                                    <style jsx>{`
               .container {
                 margin: 50px;
               }
               p {
-                color: gray;
+                color: blue;
               }
               textarea {
                 width: 600px;
                 height: 150px;
               }
-              `}</style>
-      </div>
-    );
-  }
-}
-
-class FirstPostRightGrid extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <Container maxWidth="md">
-          <label>
-            Nome do item
-          </label>
-          <form>
-            <textarea />
-          </form>
-        </Container>
-
-        <Container maxWidth="md">
-          <label>
-            Descrição do anúncio
-          </label>
-          <form>
-            <textarea />
-          </form>
-          <p>
-            Atenção: Não adicione seus dados de contato à descrição pública do
-            anúncio. Ao invés disso, adicione uma forma de contato ao seu perfil
-            de usuário aqui.
-          </p>
-        </Container>
-        <style jsx>{`
-              .container {
-                margin: 50px;
-              }
-              p {
-                color: gray;
-              }
-              textarea {
-                width: 600px;
-                height: 150px;
-              }
-              `}</style>
-      </div>
-    );
-  }
+            `}</style>
+                                </div>
+        );
+    }
 }
 
 
